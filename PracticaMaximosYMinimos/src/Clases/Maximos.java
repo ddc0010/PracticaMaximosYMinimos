582d7291c59e69d4019dda43e70dd92b50864902
ddc0010 <ddc0010@alu.medac.es>
2024-06-04 17:29:32 +0200

    Metodo MaximoDe3EnterosPositivos creado

--- PracticaMaximosYMinimos/src/Clases/Maximos.java
+++ PracticaMaximosYMinimos/src/Clases/Maximos.java
@@ -22,5 +22,29 @@ public class Maximos {
 		}
 		return max;
 	}
+	
+	/**
+	 * Metodo que devuelve el valor maximo de tres numeros enteros positivos
+	 * 
+	 * @param num1 Primer numero entero
+	 * @param num2 Segundo numero entero 
+	 * @param num3 Tercer numero entero
+	 * @return El valor maximo de los dos numeros 
+	 * @throws IllegalArgumentException Si alguno de los valores pasados por parámetro es menor que cero
+	 */
+	public static int MaximoDe3EnterosPositivos(int num1, int num2, int num3) {
+		if (num1 < 0 || num2 < 0 || num3 < 0) {
+			throw new IllegalArgumentException("Los numeros deben ser mayores o iguales que cero");
+		}
+		int max = num1; 
+		if (num2 > max) {
+			max = num2; 
+		}
+		if (num3 > max) {
+			max = num3;
+		}
+		return max;
+		
+	}
 
 }

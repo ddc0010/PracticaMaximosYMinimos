93e1b085fa40578674415915def30fbf2d93f88a
ddc0010 <ddc0010@alu.medac.es>
2024-06-04 18:00:18 +0200

    Test Minimos Creado

--- PracticaMaximosYMinimos/src/Tests/TestMinimos.java
+++ PracticaMaximosYMinimos/src/Tests/TestMinimos.java
@@ -4,11 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Clases.Minimos;
+
 class TestMinimos {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void MinimoDe2Numeros() {
+		int num1 = 10; 
+		int num2 = 4;
+		int minimoEsperado = 4;
+		int minimoObtenido = Minimos.MinimoDe2EnterosPositivos(num1, num2);
+	}
+	
+	@Test
+	public void MaximoDe3Numeros() {
+		int num1 = 9; 
+		int num2 = 3; 
+		int num3 = 7;
+		int minimoEsperado = 3;
+		int minimoObtenido = Minimos.Minimode3EnterosPositivos(num1,num2,num3);
 	}
 
 }
